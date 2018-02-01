db93a9fc1fad53f08d7e697faf460448b116a399
UltramanX <minoboyes@gmail.com>
2018-02-02 00:23:29 +0100

    add user/stock association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180131101425) do
+ActiveRecord::Schema.define(version: 20180201211135) do
 
   create_table "stocks", force: :cascade do |t|
     t.string "ticker"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 20180131101425) do
     t.decimal "market_volume"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "user_stocks", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "stock_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["stock_id"], name: "index_user_stocks_on_stock_id"
+    t.index ["user_id"], name: "index_user_stocks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
