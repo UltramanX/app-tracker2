67897f2ecfed4efac9ac692910c869c19c11a53f
UltramanX <minoboyes@gmail.com>
2018-02-01 20:44:56 +0100

    fixed error msgs

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -5,19 +5,18 @@ class StocksController < ApplicationController
        @stock = Stock.new_from_lookup(params[:stock])
        if @stock
        	respond_to do |format|
-        format.js { render partial: 'users/schresult' }
+          format.js { render partial: 'users/schresult' }
         #'users/my_portfolio' -http req 
         #json: @stock - test
        end
         
        else
-        flash[:danger] = "Incorrect ticker entered"
-        redirect_to my_portfolio_path
+        flash.now[:danger] = "Incorrect ticker entered"
+        render partial: 'users/schresult'
        end
  	   else
- 	    flash[:danger] = "Empty ticker entered"
- 	    redirect_to my_portfolio_path
- 	   end
-  	end
-
+ 	    flash.now[:danger] = "Empty ticker entered"
+ 	    render partial: 'users/schresult'
+ 	  end
+  end
 end
